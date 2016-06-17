34e6ea59c9c78f0a6fd4839a18953b338e55f757
Craig Shoemaker <craig@craigshoemaker.net>
2016-06-17 11:46:41 -0700

    add typings

---
+++ typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/angular-resource/index.d.ts" />
+/// <reference path="globals/angular-route/index.d.ts" />
+/// <reference path="globals/angular/index.d.ts" />
+/// <reference path="globals/jquery/index.d.ts" />
