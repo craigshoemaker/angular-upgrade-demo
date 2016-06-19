9408d4486b52dc3be1f9e0ad72fc583fd4fa3ca3
Craig Shoemaker <craig@craigshoemaker.net>
2016-06-18 22:13:11 -0700

    bootstrapping ng2

--- app/ng1-ng2/main.ts
+++ app/ng1-ng2/main.ts
@@ -1,5 +1,7 @@
 import { UpgradeAdapter } from '@angular/upgrade';
 import { Ng2Service } from './ng2.service';
+import { AppComponent } from './app.component';
+import { bootstrap }    from '@angular/platform-browser-dynamic';
 
 let upgradeAdapter = new UpgradeAdapter();
 upgradeAdapter.addProvider(Ng2Service);
@@ -7,4 +9,5 @@ upgradeAdapter.addProvider(Ng2Service);
 angular.module('ng1ng2')
        .factory('ng2Service', upgradeAdapter.downgradeNg2Provider(Ng2Service));
 
-upgradeAdapter.bootstrap(document.documentElement, ['ng1ng2']);
+upgradeAdapter.bootstrap(document.documentElement, ['ng1ng2', AppComponent]);
+bootstrap(AppComponent);
