9b445061b9cacf14a381f3cb5d6d0350a9460918
Craig Shoemaker <craig@craigshoemaker.net>
2016-06-23 08:32:23 -0700

    even moar cleanup!

--- app/ng1-ng2/ng1-list.component.ts
+++ app/ng1-ng2/ng1-list.component.ts
@@ -1,15 +1,13 @@
 angular.module('ng1ng2').component('ng1List', {
     templateUrl: 'app/ng1-ng2/ng1-list.component.html',
     controller: class ng1ListController {
-        title: string;
+        title: string = 'ng1 component';
         ng1Data: any[];
         ng2Data: any[];
 
         static $inject = ['ng2Service', 'ng1Service'];
 
         constructor(ng2Service: any, ng1Service: any) {
-
-            this.title = 'ng1 component';
 
             ng1Service.getAll().then((data) => {
                 this.ng1Data = data;
--- app/ng1/ng1-list.component.ts
+++ app/ng1/ng1-list.component.ts
@@ -1,6 +1,5 @@
 angular.module('app').component('ng1List', {
     templateUrl: 'app/ng1/ng1-list.component.html',
-    bindings: {},
     controller: class ng1ListController {
         title: string = 'ng1 component';
         data: any[];
